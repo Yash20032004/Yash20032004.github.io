a32e34257d94ba6b9a802081ece35d4696a7927a
YASH PATEL <yash.patel16@dcmail.ca>
2022-01-21 23:12:03 -0500

    added article paragraph and main paragraph(not working!)

--- scripts/app.js
+++ scripts/app.js
@@ -6,10 +6,31 @@
    function DisplayHomePage()
    {
       console.log("Home page");
+
       let AboutUsButton = document.getElementById("AboutUsButton");
-      AboutUsButton.addEventListener("click", function(){
-         location.href = "about.html"
+      AboutUsButton.addEventListener("click", function()
+      {
+         location.href = "about.html";
       });
+
+      //step-1 - get a reference to entry point.
+      let MainContent = document.getElementsByTagName("main")[0];
+      let DocumentBody = document.body;
+    
+   
+      let MainParagraph = document.createElement("p");    
+      let Article = document.createElement("article");
+      let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is article paragraph</p>`;
+
+      // step-3 configure
+      MainParagraph.setAttribute("id", "MainParagraph");
+      MainParagraph.setAttribute("class", "mt-3");
+      MainParagraph.textContent = "This is main paragraph";
+      // step-4 performation
+      MainContent.appendChild(MainParagraph);
+
+      Article.innerHTML = ArticleParagraph;
+      DocumentBody.appendChild(Article);
 
 
    }
