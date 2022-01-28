b5a0f7a57692334bf46e7ac128eb1afe2cb60cca
YASH PATEL <yash.patel16@dcmail.ca>
2022-01-28 16:40:21 -0500

    updating changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,42 +1,57 @@
-// IIFE -- Immediately Invoked Function Expression
-
 (function()
 {
 
-   function DisplayHomePage()
-   {
-      console.log("Home page");
+    function DisplayHomePage()
+    {
+        console.log("Home page");
+        let AboutUsButton = document.getElementById("AboutUsButton");
+        
+        AboutUsButton.addEventListener("click", function()
+        {
+            location.href = "about.html";
+        });
 
-      let AboutUsButton = document.getElementById("AboutUsButton");
-      AboutUsButton.addEventListener("click", function()
-      {
-         location.href = "about.html";
-      });
-
+        
       //step-1 - get a reference to entry point.
-      let MainContent = document.getElementsByTagName("main")[0];
-      let DocumentBody = document.body;
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+        
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+
+       // step-3 configure
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+
+        MainParagraph.textContent = "This is main paragraph";
+
+        // step-4 performation
+        Article.setAttribute("class", "container");
+
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+
+        console.log("HOME PAGE!!");
+
+        //testing contact class
+        let Yash = new Contact("Yash", "289-200-2800", "yash.patel16@dcmail.ca");
+        console.log(Yash.toString());
+
+
+        
+
+       
+        
+
+
+    }
     
-   
-      let MainParagraph = document.createElement("p");    
-      let Article = document.createElement("article");
-      let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is article paragraph</p>`;
-
-      // step-3 configure
-      MainParagraph.setAttribute("id", "MainParagraph");
-      MainParagraph.setAttribute("class", "mt-3");
-      MainParagraph.textContent = "This is main paragraph";
-      // step-4 performation
-      MainContent.appendChild(MainParagraph);
-
-      Article.innerHTML = ArticleParagraph;
-      DocumentBody.appendChild(Article);
-
-
-   }
-
    function DisplayProductspage(){
       console.log("Product page");
+     
 
    }
 
@@ -49,19 +64,72 @@
       console.log("About page");
 
    }
+
    function DisplayContactpage(){
       console.log("Contact page");
+      let sendButton = document.getElementById("sendButton");
+      let subscribecheckbox = document.getElementById("subscribecheckbox");
+
+      sendButton.addEventListener("click", function(event){
+         
+         //event.preventDefault();
+         if(subscribecheckbox.checked)
+         {
+            let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+            if(contact.serialize())
+            {
+               let key = contact.FullName.substring(0,1) + Date.now();
+               localStorage.setItem(key, contact.serialize());
+            }
+         }
+      });
 
    }
 
-   function Start()
-   {
-        console.log("App Started!");
+   function DisplayContactListpage(){
+      console.log("Contact-list page");
+      if(localStorage.length > 0)
+      {
+         let contactList = document.getElementById("contactList");
+         let data = "";
+         let keys = Objects.keys(localStorage);
+         let index = 1;
+         for(const key of keys)
+         {
+            let contactData = localStorage.getItem(key);
+            let contact = new Contact(); // empty object
+            contact.deserialize(contactData);
+            data += `<tr>
+            <th scope="row" class="text-center">${index}</th>
+            <td>${contact.FullName}</td>
+            <td>${contact.ContactNumber}</td>
+            <td>${contact.EmailAddress}</td>
+            <td></td>
+            <td></td>
+            </tr>`
+            ;
+                
+            index++;
+            
+            
+         }
+         contactList.innerHTML = data;
+      }
+
+   }
+
+
+  
+
+    // named function
+    function Start()
+    {
+        console.log("App Started!!");
 
         switch(document.title)
         {
-           case "Home":
-              DisplayHomePage();
+            case "Home":
+               DisplayHomePage();
                break;
             case "Our Products":
               DisplayProductspage();
@@ -75,12 +143,11 @@
             case "Contact us":
               DisplayContactpage();
                break;
+            case "Contact-List":
+             DisplayContactListpage();
         }
+    }
 
+    window.addEventListener("load", Start);
 
-      
-   } 
-
-   
-   window.addEventListener("load",Start);
 })();
